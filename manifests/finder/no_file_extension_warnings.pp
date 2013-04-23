2f7ffba862c60efc34b7e232a208f1ef8935e1bb
Gabe Blair <gabe@castironcoding.com>
2013-04-23 01:46:27 -0700

    Disable file extension warnings

---
+++ manifests/finder/no_file_extension_warnings.pp
@@ -0,0 +1,12 @@
+# Public: Disable file extension warnings.
+class osx::finder::no_file_extension_warnings {
+  include osx::finder
+
+  boxen::osx_defaults { 'Disable file extension change warnings':
+    user   => $::boxen_user,
+    domain => 'com.apple.finder',
+    key    => 'FXEnableExtensionChangeWarning',
+    value  => false,
+    notify => Exec['killall Finder'];
+  }
+}
